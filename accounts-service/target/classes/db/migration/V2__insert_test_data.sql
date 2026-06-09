85cd1bc9c197b455e2b92d3fa6a997fb57a43620
Борисов Андрей <ayborisov1@mts.ru>
2026-06-09 14:06:00 +0300

    Add .gitignore to exclude target/ and .idea/ directories

--- accounts-service/target/classes/db/migration/V2__insert_test_data.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO accounts_schema.accounts (login, full_name, birthdate, sum, created_at)
-VALUES
-    ('ivanov', 'Иванов Иван', '1990-05-15', 5000, CURRENT_TIMESTAMP),
-    ('petrov', 'Петров Петр', '1985-08-20', 3000, CURRENT_TIMESTAMP),
-    ('sidorov', 'Сидоров Сидор', '1995-03-10', 2000, CURRENT_TIMESTAMP),
-    ('smirnov', 'Смирнов Алексей', '2000-12-01', 1000, CURRENT_TIMESTAMP)
-    ON CONFLICT (login) DO NOTHING;
--- cash-service/target/classes/db/migration/V1__create_cash_schema.sql
+++
@@ -1,1 +0,0 @@
-CREATE SCHEMA IF NOT EXISTS cash_schema;
