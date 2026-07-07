c185ce2b8535bfc300cdd262569824414983d7ba
Борисов Андрей <ayborisov1@mts.ru>
2026-07-07 20:21:29 +0300

    my_project_new_features_branch_s11

--- cash-service/src/main/java/ru/yandex/practicum/cash/service/NotificationClient.java
+++ cash-service/src/main/java/ru/yandex/practicum/cash/service/NotificationClient.java
@@ -2,35 +2,25 @@ package ru.yandex.practicum.cash.service;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Service;
-import org.springframework.web.reactive.function.client.WebClient;
-import ru.yandex.practicum.cash.dto.NotificationRequestDto;
+import ru.yandex.practicum.cash.kafka.NotificationEventProducer;
 
 @Service
 public class NotificationClient {
 
     private static final Logger log = LoggerFactory.getLogger(NotificationClient.class);
 
-    private final WebClient notificationsWebClient;
+    private final NotificationEventProducer notificationEventProducer;
 
-    public NotificationClient(@Qualifier("notificationsWebClient") WebClient notificationsWebClient) {
-        this.notificationsWebClient = notificationsWebClient;
+    public NotificationClient(NotificationEventProducer notificationEventProducer) {
+        this.notificationEventProducer = notificationEventProducer;
     }
 
     public void sendNotification(String userLogin, String message, String type) {
         try {
-            var request = new NotificationRequestDto(userLogin, message, type);
-            notificationsWebClient.post()
-                    .uri("/api/notifications")
-                    .bodyValue(request)
-                    .retrieve()
-                    .toBodilessEntity()
-                    .doOnSuccess(response -> log.info("Notification sent to {}: {}", userLogin, message))
-                    .doOnError(error -> log.error("Failed to send notification: {}", error.getMessage()))
-                    .subscribe();
+            notificationEventProducer.sendNotification(userLogin, message, type);
         } catch (Exception e) {
-            log.error("Error sending notification: {}", e.getMessage());
+            log.error("Error sending notification via Kafka: {}", e.getMessage());
         }
     }
 }
